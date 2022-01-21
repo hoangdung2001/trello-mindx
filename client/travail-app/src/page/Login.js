8b833dfb8a25fbf5956c724209d88dd7d48c5a00
leviethoang082001 <leviethoang082001@gmail.com>
2022-01-21 15:14:14 +0700

    fix login

--- client/travail-app/src/page/Login.js
+++ client/travail-app/src/page/Login.js
@@ -47,6 +47,7 @@ const Login = () => {
             ></input>
             <input
               className="password input"
+              type={"password"}
               placeholder="Nhập mật khẩu"
               name="password"
             ></input>
